6d410bac7311df8dc754e5e023590999b15d2028
skfahadibrahim <ibrahimsk7020@gmail.com>
2026-05-30 19:07:52 +0530

    done

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -19,7 +19,7 @@ export class AuthService {
     private jwtService: JwtService,
   ) {
     this.transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
+      host: "smtp-relay.brevo.com",
       port: 587,
       secure: false,
       auth: {
